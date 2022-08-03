8ea7d7c54ac5750db74bd397b869cfed16e64766
Felipe <oliveira.felipe28@gmail.com>
2022-08-03 17:39:53 -0300

    Feat: Alteração no REACT DOM, implementação do texto principal da página HOME e criação de links entre páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import Header from "./components/Header/Header"
+import MainSection from "./components/MainSection/MainSection"
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Cars from './components/Cars/Cars'
 
 function App() {
 
 
   return (
-    <div>
-      <Header />
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<MainSection />} />
+          <Route exact path="/cars" element={<Cars />} />
+        </Routes>
+    </Router>
     </div>
   )
 }
 
 export default App
+
+
+
