9bceedb0f29f32a495c42ac7f904433b8013c0e2
Felipe <oliveira.felipe28@gmail.com>
2022-08-05 00:27:49 -0300

    Fix: Alteração na estrutura para deploy

--- src/components/Cars/Cars.tsx
+++ src/components/Cars/Cars.tsx
@@ -6,21 +6,19 @@ import { api } from '../../services/api/fake-api'
 import { useState } from 'react'
 import { orderArray } from '../../services/functions/functions'
 import { car } from '../../services/interfaces/car'
-import { ShopCartContext } from '../../context/ShopCart'
-import { useContext } from 'react'
+
 
 
 export default function Cars() {
 
 
   const [apiCopy, setApiCopy] = useState<car[]>([...api])
-  const {products, setProducts} = useContext(ShopCartContext)
 
   return (
     <div className='cars-div'>
       <Header />
       <Form
-        onSubmit={(minValue, maxValue, select) => {
+        onSubmit={(minValue: number, maxValue: number, select: string) => {
           setApiCopy(orderArray(select, api))
           return setApiCopy(api.filter(car => car.value >= minValue && car.value <= maxValue)
           )
